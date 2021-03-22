bf05e8070113ce28245750f023edfa147faf099e
Riyanwar Setiadi <riyanwarsetiadi0605@gmail.com>
2021-03-22 10:21:09 +0700

    Update Struktur Project

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,17 @@ import "reflect-metadata";
 import {createConnection} from "typeorm";
 import express, {Application} from "express";
 import bodyParser from "body-parser";
-import {Server} from "typescript-rest";
 import cors from "cors";
 import helmet from "helmet";
 import * as dotenv from "dotenv";
+import routes from "routes/route";
 dotenv.config();
 
 const app : Application = express();
 createConnection().then(async (connection)=>{
   app.use(helmet());
   app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({extended: true}));
-  Server.buildServices(app);
+  app.use(bodyParser.urlencoded({extended: true}));  
   app.listen(process.env.PORT,()=>{
     console.log(`Server Running at Port : ${process.env.PORT}`)
   })
